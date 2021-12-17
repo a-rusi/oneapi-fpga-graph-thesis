e1d2f9354ed15f9300234f6d92cee627848bbde2
a-rusi <alejandro@muttdata.ai>
2021-12-17 13:58:16 -0300

    facebook graph

--- parallel_bfs.cpp
+++ parallel_bfs.cpp
@@ -6,29 +6,56 @@ using namespace sycl;
 using namespace std;
 
 #include "dpc_common.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "sequential_bfs.h"
 
 #define NS (1000000000.0) // number of nanoseconds in a second
 
-void bfs()
+struct edge
 {
-    //simple graph init
-    int nodes = 10000;
-    vector<vector<int>> graph(nodes, vector<int>(nodes, -1));
-    for (int i = 0; i < nodes; i++)
+    int n;
+    int m;
+};
+
+vector<vector<int>> create_graph(string filename)
+{
+
+    std::ifstream file;
+    file.open(filename);
+    int nodes;
+    int n;
+    int m;
+
+    file >> nodes;
+    vector<edge> edges;
+    while (file >> n >> m)
     {
-        int amount_of_edges = (rand() % nodes + 1) / 2;
-        for (int j = 0; j < amount_of_edges; j++)
-        {
-            int other_node = rand() % nodes + 1;
-            if (other_node != i)
-            {
-                graph[i][other_node] = other_node;
-            }
-        }
+        edges.push_back({n, m});
+    }
+    file.close();
+
+    vector<vector<int>> graph(nodes, vector<int>(0));
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int start = edges.at(i).n;
+        int end = edges.at(i).m;
+        graph[start].push_back(end);
     }
 
+    return graph;
+}
+
+vector<int> parallel_bfs(vector<vector<int>> &graph)
+
+{
+    //simple graph init
+    int nodes = graph.size();
+
     // profiling utils
-    chrono::high_resolution_clock::time_point t1_host, t2_host;
+    chrono::high_resolution_clock::time_point t1_host,
+        t2_host;
     sycl::cl_ulong t1_kernel, t2_kernel;
     double time_kernel;
     auto my_property_list = property_list{sycl::property::queue::enable_profiling()};
@@ -39,7 +66,7 @@ void bfs()
 #else
     sycl::INTEL::fpga_selector device_selector;
 #endif
-    queue q(device_selector, NULL, my_property_list);
+    sycl::queue q(device_selector, NULL, my_property_list);
     platform platform = q.get_context().get_platform();
     device my_device = q.get_device();
     std::cout << "Platform name: " << platform.get_info<sycl::info::platform::name>().c_str() << std::endl;
@@ -65,52 +92,55 @@ void bfs()
 
     bool all_nodes_visited = false;
     auto nodes_range = range(frontier.size());
-    while (!all_nodes_visited)
-    {
-        queue_event = q.submit([&](handler &h) {
-            accessor graph_access(graph_buffer, h, read_only);
-            accessor parents_access(parent_buffer, h);
-            accessor frontier_access(frontier_buffer, h, read_only);
-            accessor next_frontier_access(next_frontier_buffer, h, write_only);
-            accessor finish_vector_access(finish_vector_buffer, h, write_only);
 
-            // iterate over the frontier, update node parents
+    queue_event = q.submit([&](handler &h) {
+        accessor graph_access(graph_buffer, h, read_only);
+        accessor parents_access(parent_buffer, h);
+        accessor frontier_access(frontier_buffer, h);
+        accessor next_frontier_access(next_frontier_buffer, h, write_only);
+        accessor finish_vector_access(finish_vector_buffer, h, write_only);
 
-            h.parallel_for(nodes_range, [=](auto i) {
-                if (frontier_access[i] != -1)
+        // iterate over the frontier, update node parents
+
+        h.single_task([=]() {
+            bool all_nodes_visited = false;
+            for (int iteration = 0; iteration < nodes; iteration++)
+            {
+                if (!all_nodes_visited)
                 {
-                    for (auto &neighbor : graph_access[i])
+                    all_nodes_visited = true;
+                    for (int i = 0; i < nodes; i++)
                     {
-                        if (neighbor != -1 && parents_access[neighbor] == -1)
+                        if (frontier_access[i] != -1)
                         {
-                            parents_access[neighbor] = i;
-                            next_frontier_access[neighbor] = 1;
+                            for (auto &neighbor : graph_access[i])
+                            {
+                                if (neighbor != -1 && parents_access[neighbor] == -1)
+                                {
+                                    parents_access[neighbor] = i;
+                                    next_frontier_access[neighbor] = 1;
+                                }
+                            }
                         }
                     }
+                    for (int i = 0; i < nodes; i++)
+                    {
+                        if (parents_access[i] == -1)
+                        {
+                            all_nodes_visited = false;
+                        }
+                    }
+
+                    // swap frontier vectors
+                    for (int i = 0; i < nodes; i++)
+                    {
+                        frontier_access[i] = next_frontier_access[i];
+                        next_frontier_access[i] = -1;
+                    }
                 }
-            });
-        });
-        // check if we visited all nodes and algorithm is done
-        host_accessor read_parent(parent_buffer, read_only);
-        host_accessor host_frontier(frontier_buffer);
-        host_accessor host_next_frontier(next_frontier_buffer);
-        all_nodes_visited = true;
-        for (int i = 0; i < nodes; i++)
-        {
-            if (read_parent[i] == -1)
-            {
-                all_nodes_visited = false;
             }
-        }
-
-        // swap frontier vectors
-        for (int i = 0; i < nodes; i++)
-        {
-            host_frontier[i] = host_next_frontier[i];
-            host_next_frontier[i] = -1;
-        }
-    }
-
+        });
+    });
     q.wait();
 
     // Report kernel execution time and throughput
@@ -124,9 +154,25 @@ void bfs()
     {
         std::cout << read_parent[i] << ",";
     }
+
+    return parent;
 }
 
 int main()
 {
-    bfs();
+    vector<vector<int>> graph = create_graph("facebook_combined.txt");
+    vector<int> parallel_result = parallel_bfs(graph);
+    vector<int> sequential_result = sequential_bfs(graph);
+    for (int i = 0; i < parallel_result.size(); i++)
+    {
+        int parent = parallel_result[i];
+        if (parent != i && parent != -1)
+        {
+            vector<int> parent_edges = graph[parent];
+            if (std::find(parent_edges.begin(), parent_edges.end(), i) == parent_edges.end())
+            {
+                std::cout << "Invalid parent found!" << std::endl;
+            }
+        }
+    }
 }
